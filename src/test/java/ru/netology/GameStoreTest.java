814cd15098e1aa02d7e3aac8eaa65231cbd01483
Andrei Bezrukov <bezrukov.andrey86@gmail.com>
2022-10-25 22:46:21 +0500

    Add tests in gamestore

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -91,4 +91,45 @@ public class GameStoreTest {
 
     }
 
+    @Test
+    public void playedTimeManyGamesTest() {
+
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Кольца", "Аркады");
+        Game game3 = store.publishGame("Утки", "Стратегия");
+
+        Player player = new Player("Andrei");
+        player.installGame(game1);
+        player.installGame(game2);
+        player.installGame(game3);
+        player.play(game1, 1);
+        player.play(game2, 4);
+        player.play(game3, 7);
+
+        int expected = 12;
+        int actual = store.getSumPlayedTime();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void noMostPlayedPlayerTest() {
+
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player1 = new Player("Andrei");
+        Player player2 = new Player("Evgenia");
+        player1.installGame(game);
+        player2.installGame(game);
+
+        String expected = null;
+        String actual = store.getMostPlayer();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
 }
